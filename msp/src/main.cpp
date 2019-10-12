ccf501308b89c8dcce0c776462cbd9b5f6e4a9ca
Simon Wälti <s.waelti@icloud.com>
2019-10-12 10:15:26 +0200

    submodule dji-onboardsdk added

---
+++ msp/src/main.cpp
@@ -0,0 +1,17 @@
+// System Includes
+#include <cmath>
+#include <stdio.h>
+
+// DJI OSDK includes
+#include "dji_status.hpp"
+#include <dji_vehicle.hpp>
+
+
+int main() {
+	printf("%s", "hello world");
+	return 1;
+}
+
+
+
+
