2a2e7a902aea5af570139f31c003f559c79b64ee
Samuel Ackermann <ackes3@bfh.ch>
2019-12-19 11:49:58 +0100

    Add base64-encoding function

--- src/modules/controller/controller_mission.cpp
+++ src/modules/controller/controller_mission.cpp
@@ -330,3 +330,22 @@ MspController::Mission::validateMissionItems() {
         context->setState(&context->stateIdle);
     }*/
 }
+
+typedef unsigned char uchar;
+static const std::string b = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";//=
+static std::string base64_encode(const std::string &in) {
+    std::string out;
+
+    int val=0, valb=-6;
+    for (uchar c : in) {
+        val = (val<<8) + c;
+        valb += 8;
+        while (valb>=0) {
+            out.push_back(b[(val>>valb)&0x3F]);
+            valb-=6;
+        }
+    }
+    if (valb>-6) out.push_back(b[((val<<8)>>(valb+8))&0x3F]);
+    while (out.size()%4) out.push_back('=');
+    return out;
+}
